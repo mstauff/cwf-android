30345762af61313d6d6131505fdce174d3aab388
blanchardb <bblanchard12@yahoo.com>
2016-12-22 07:49:13 -0700

    updated Org to have displayOrder, child orgs, and callings.

--- app/src/main/java/org/ldscd/callingworkflow/model/Org.java
+++ app/src/main/java/org/ldscd/callingworkflow/model/Org.java
@@ -1,5 +1,7 @@
 package org.ldscd.callingworkflow.model;
 
+import java.util.List;
+
 /**
  * Represents an auxiliary.  i.e. primary, R.S. or E.Q.
  */
@@ -8,13 +10,20 @@ public class Org {
     long id;
     String orgName;
     int orgTypeId;
+    int displayOrder;
+    List<Org> children;
+    List<Calling> callings;
 
     /* Constructors */
     public Org() {}
-    public Org(long id, String orgName, int orgTypeId) {
+
+    public Org(long id, String orgName, int orgTypeId, int displayOrder, List<Org> children, List<Calling> callings) {
         this.id = id;
         this.orgName = orgName;
         this.orgTypeId = orgTypeId;
+        this.displayOrder = displayOrder;
+        this.children = children;
+        this.callings = callings;
     }
 
     /* Properties */
@@ -26,4 +35,13 @@ public class Org {
 
     public int getOrgTypeId() { return orgTypeId; }
     public void setOrgTypeId(int orgTypeId) { this.orgTypeId = orgTypeId; }
+
+    public int getDisplayOrder() { return displayOrder; }
+    public void setDisplayOrder(int displayOrder) { this.displayOrder = displayOrder; }
+
+    public List<Org> getChildren() { return children; }
+    public void setChildren(List<Org> children) { this.children = children; }
+
+    public List<Calling> getCallings() { return callings; }
+    public void setCallings(List<Calling> callings) { this.callings = callings; }
 }
