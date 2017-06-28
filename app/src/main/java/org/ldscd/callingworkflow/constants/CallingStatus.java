a838d21a8d6f2d35b0b66dfe8a4d214c8a89ede7
blanchardb <bblanchard12@yahoo.com>
2017-06-28 09:31:23 -0600

    Status Changes #48

--- app/src/main/java/org/ldscd/callingworkflow/constants/CallingStatus.java
+++ app/src/main/java/org/ldscd/callingworkflow/constants/CallingStatus.java
@@ -7,29 +7,31 @@ public enum CallingStatus {
     PROPOSED(0 , "Proposed"),
     SUBMITTED(1, "Submitted"),
     APPROVED(2, "Approved"),
-    REJECTED(3, "Rejected"),
+    NOT_APPROVED(3, "Not Approved"),
     ON_HOLD(4, "On Hold"),
-    APPT_SET(5, "Appointment Set"),
+    APPOINTMENT_SET(5, "Appointment Set"),
     EXTENDED(6, "Extended"),
     ACCEPTED(7, "Accepted"),
-    DECLINED(8, "Declined"),
+    REFUSED(8, "Refused"),
     SUSTAINED(9, "Sustained"),
     SET_APART(10, "Set Apart"),
     RECORDED(11, "Recorded"),
-    UNKNOWN(12, "Unknown");
+    UNKNOWN(12, "Unknown"),
+    READY_TO_SUSTAIN(13, "Ready to Sustain"),
+    RESUBMIT(14, "Resubmit");
 
-    private final String status;
+    private final String printName;
     private final int position;
-    private CallingStatus(int position, String status) {
+    private CallingStatus(int position, String printName) {
         this.position = position;
-        this.status = status;
+        this.printName = printName;
     }
 
     /**
      * @return The string representation of this element in the enumeration.
      */
-    public String getStatus() {
-        return this.status;
+    public int getStatus() {
+        return this.position;
     }
 
     private static final Map<String, CallingStatus> lookup = new HashMap<String, CallingStatus>();
@@ -45,6 +47,6 @@ public enum CallingStatus {
     }
 
     @Override public String toString() {
-        return this.status;
+        return this.printName;
     }
 }
