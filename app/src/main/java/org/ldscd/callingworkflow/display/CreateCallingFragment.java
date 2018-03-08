08ac237e86ed822d0cb758cb31a4d5e21d3dcac2
ashepherd13 <admshepherd@gmail.com>
2018-03-08 14:49:27 -0700

    Position requirements conflict warning CreateCalling page, ExpandableOrgsList child selection bug

--- app/src/main/java/org/ldscd/callingworkflow/display/CreateCallingFragment.java
+++ app/src/main/java/org/ldscd/callingworkflow/display/CreateCallingFragment.java
@@ -1,13 +1,18 @@
 package org.ldscd.callingworkflow.display;
 
+import android.app.AlertDialog;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -21,6 +26,8 @@ import org.ldscd.callingworkflow.model.LdsUser;
 import org.ldscd.callingworkflow.model.Member;
 import org.ldscd.callingworkflow.model.Org;
 import org.ldscd.callingworkflow.model.Position;
+import org.ldscd.callingworkflow.model.PositionMetaData;
+import org.ldscd.callingworkflow.model.PositionRequirements;
 import org.ldscd.callingworkflow.model.permissions.constants.Permission;
 import org.ldscd.callingworkflow.web.DataManager;
 
@@ -40,6 +47,7 @@ public class CreateCallingFragment extends Fragment implements MemberLookupFragm
     private View view;
     private Org parentOrg;
     private Member proposedMember;
+    private Position position;
     private SubFragmentOpenListener subFragmentListener;
     boolean resetProposedStatus = false;
     boolean canViewPriesthood;
@@ -60,7 +68,6 @@ public class CreateCallingFragment extends Fragment implements MemberLookupFragm
         if(proposedMember != null) {
             proposedId = proposedMember.getIndividualId();
         }
-        Position position = (Position)positionDropdown.getSelectedItem();
         CallingStatus status = (CallingStatus) statusDropdown.getSelectedItem();
         String notes = notesBox.getText().toString();
 
@@ -97,8 +104,20 @@ public class CreateCallingFragment extends Fragment implements MemberLookupFragm
             //Position Dropdown
             List<Position> potentialPositions = parentOrg.potentialNewPositions();
             positionDropdown = (Spinner) view.findViewById(R.id.new_calling_position_dropdown);
-            PositionArrayAdapter positionAdapter = new PositionArrayAdapter<Position>(getContext(), android.R.layout.simple_list_item_1, potentialPositions);
+            final PositionArrayAdapter positionAdapter = new PositionArrayAdapter<Position>(getContext(), android.R.layout.simple_list_item_1, potentialPositions);
             positionDropdown.setAdapter(positionAdapter);
+            position = (Position)positionDropdown.getSelectedItem();
+            positionDropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                    position = (Position)positionAdapter.getItem(i);
+                    checkPositionRequirements();
+                }
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+
+                }
+            });
         }
 
         //Status Dropdown
@@ -122,6 +141,7 @@ public class CreateCallingFragment extends Fragment implements MemberLookupFragm
             TextView proposedMemberTextView = (TextView) view.findViewById(R.id.member_lookup_name);
             proposedMemberTextView.setText(proposedMember.getFormattedName());
         }
+        checkPositionRequirements();
         ImageButton searchButton = (ImageButton) view.findViewById(R.id.member_lookup_button);
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -144,6 +164,34 @@ public class CreateCallingFragment extends Fragment implements MemberLookupFragm
 
     }
 
+    private void checkPositionRequirements() {
+        ImageView memberWarningIcon = (ImageView) view.findViewById(R.id.member_selection_warning);
+        memberWarningIcon.setVisibility(View.GONE);
+        if(proposedMember != null) {
+            PositionMetaData metaData = dataManager.getPositionMetadata(position.getPositionTypeId());
+            PositionRequirements requirements = metaData != null ? metaData.getRequirements() : null;
+            if(requirements != null && !requirements.meetsRequirements(proposedMember)) {
+                memberWarningIcon.setVisibility(View.VISIBLE);
+                memberWarningIcon.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        View dialogView = getActivity().getLayoutInflater().inflate(R.layout.warning_dialog_text, null);
+                        TextView messageTextView = (TextView)dialogView.findViewById(R.id.warning_message);
+                        messageTextView.setText(R.string.missing_requirement_warning);
+                        AlertDialog alertDialog = new AlertDialog.Builder(getContext())
+                                .setView(dialogView)
+                                .setPositiveButton(R.string.ok, null)
+                                .show();
+                        Button positiveButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams)positiveButton.getLayoutParams();
+                        layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                        positiveButton.setLayoutParams(layoutParams);
+                    }
+                });
+            }
+        }
+    }
+
     private void openMemberLookup() {
         MemberLookupFragment memberLookupFragment = new MemberLookupFragment();
         memberLookupFragment.setMemberLookupListener(this);
@@ -152,7 +200,6 @@ public class CreateCallingFragment extends Fragment implements MemberLookupFragm
         if(proposedMember != null && proposedMember.getIndividualId() > 0) {
             args.putLong(CallingDetailSearchFragment.INDIVIDUAL_ID, proposedMember.getIndividualId());
         }
-        Position position = (Position)positionDropdown.getSelectedItem();
         args.putInt(MemberLookupFragment.positionTypeIdName,  position != null ? position.getPositionTypeId() : 0);
         memberLookupFragment.setArguments(args);
         getFragmentManager().beginTransaction()
