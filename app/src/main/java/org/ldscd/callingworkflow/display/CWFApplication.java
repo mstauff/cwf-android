ee35a2fc37eef1cfde1d462560466674000b6fd8
blanchardb <bblanchard12@yahoo.com>
2018-05-23 13:54:33 -0600

    Removed unecessary check for SSL/HTTPS items.

--- app/src/main/java/org/ldscd/callingworkflow/display/CWFApplication.java
+++ app/src/main/java/org/ldscd/callingworkflow/display/CWFApplication.java
@@ -1,21 +1,10 @@
 package org.ldscd.callingworkflow.display;
 
-import android.annotation.SuppressLint;
 import android.app.Application;
 
 import org.ldscd.callingworkflow.dependencies.ApplicationContextModule;
 import org.ldscd.callingworkflow.dependencies.DaggerNetComponent;
 import org.ldscd.callingworkflow.dependencies.NetComponent;
-
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
 public class CWFApplication extends Application {
     private NetComponent netComponent;
@@ -24,8 +13,6 @@ public class CWFApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        handleSSLHandshake();
-
         netComponent = DaggerNetComponent.builder()
                 .applicationContextModule(new ApplicationContextModule(this))
                 .build();
@@ -33,35 +20,5 @@ public class CWFApplication extends Application {
 
     public NetComponent getNetComponent() {
         return netComponent;
-    }
-
-    @SuppressLint("TrulyRandom")
-    public static void handleSSLHandshake() {
-        try {
-            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
-                public X509Certificate[] getAcceptedIssuers() {
-                    return new X509Certificate[0];
-                }
-
-                @Override
-                public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                }
-
-                @Override
-                public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                }
-            }};
-
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(null, trustAllCerts, new SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
-                @Override
-                public boolean verify(String arg0, SSLSession arg1) {
-                    return true;
-                }
-            });
-        } catch (Exception ignored) {
-        }
     }
 }
